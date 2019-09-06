7f23dac92532cb8d2844cade2c64acc978e2aa3c
Vasiliy Mazhekin <mazhekin@gmail.com>
2019-09-06 08:42:27 +0200

    add test ci configuration

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,16 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    // browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+    browserNoActivityTimeout: 60000,
+
     singleRun: false,
     restartOnFileChange: true
   });
